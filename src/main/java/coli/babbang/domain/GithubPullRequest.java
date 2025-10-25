c9be6a675a5a9ab21061432ad095fb9f9b000b50
coli <kkwoo001021@naver.com>
2025-10-25 23:14:20 +0900

    feat: 풀리퀘스트 엔티티 구현

---
+++ src/main/java/coli/babbang/domain/GithubPullRequest.java
@@ -0,0 +1,40 @@
+package coli.babbang.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class GithubPullRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "external_id")
+    private long externalId;
+
+    @Column(name = "repo_id")
+    private long repoId;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private ReviewStatus status;
+
+    public GithubPullRequest(long externalId, long repoId, ReviewStatus status) {
+        this(null, externalId, repoId, status);
+    }
+}
