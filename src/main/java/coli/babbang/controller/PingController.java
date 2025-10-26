8441013d2bd8d6ba0969c79a78d9a79b9714275a
coli <kkwoo001021@naver.com>
2025-10-27 01:04:49 +0900

    feat: CI/CD파이프라인 구축

---
+++ src/main/java/coli/babbang/controller/PingController.java
@@ -0,0 +1,23 @@
+package coli.babbang.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class PingController {
+
+    @Value("${github.token}")
+    private String masterToken;
+
+    @Value("${webhook.url}")
+    private String webhookUrl;
+
+
+    public String pong() {
+        log.info(masterToken + "github Token");
+        log.info(webhookUrl + "webhook");
+        return "pong";
+    }
+}
